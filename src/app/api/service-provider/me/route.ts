0a646b222b9889459d783b52d19aabdece0fcbf8
Shei Goldberg <shei@sheigoldberg.com>
2024-11-29 09:44:03 +0200

    Deleted the calendar and service-provider API's which were using react query to manage data in the user interface

--- src/app/api/service-provider/me/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-import { getAuthenticatedServiceProvider } from '@/lib/server-helper';
-
-export async function GET() {
-  const result = await getAuthenticatedServiceProvider();
-  return NextResponse.json(result);
-}
